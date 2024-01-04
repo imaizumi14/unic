e067070fbc69df511e64b0b2b2e573a94e9c4241
imaizumi14 <30835848+imaizumi14@users.noreply.github.com>
2024-01-04 21:55:19 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,3 +1,5 @@
+let BoardOffsetX = 50
+let BoardOffsetY = 100
 let l1Img
 let l2Img
 let l3Img
@@ -17,16 +19,16 @@ function preload() {
 
 function setup() {
   frameRate(60)
-  createCanvas(600, 600).parent(document.getElementById('p5pos'))
+  createCanvas(500, 600).parent(document.getElementById('p5pos'))
   
-  shape = createGraphics(80, 80);
-  shape.rect(0, 0, 80, 80, 10);
-  l1Img.mask(shape)
-  l2Img.mask(shape)
-  l3Img.mask(shape)
-  l4Img.mask(shape)
-  l5Img.mask(shape)
-  l6Img.mask(shape)
+  panelShape = createGraphics(80, 80);
+  panelShape.rect(0, 0, 80, 80, 10);
+  l1Img.mask(panelShape)
+  l2Img.mask(panelShape)
+  l3Img.mask(panelShape)
+  l4Img.mask(panelShape)
+  l5Img.mask(panelShape)
+  l6Img.mask(panelShape)
 
   pointPos = 0
 }
@@ -34,15 +36,6 @@ function setup() {
 function draw() {
   update()
   drawImpl()
-}
-
-function keyPressed() {
-}
-
-function mousePressed() { 
-}
-
-function touchStarted() {
 }
 
 function update() {
@@ -54,29 +47,36 @@ function update() {
 
 function drawImpl() {
   clear()
+  drawTitle()
+  drawBoard()
+  drawPanels()
+}
 
+function drawTitle() {
   textFont('Impact')
   textSize(48)
   fill('#808080')
-  text('▲ UNICORN GAME ▼', 102, 74)
+  text('▲ UNICORN GAME ▼', 52, 74)
   fill('#ffffff')
-  text('▲ UNICORN GAME ▼', 102, 70)
+  text('▲ UNICORN GAME ▼', 52, 70)
+}
 
+function drawBoard() {
   stroke('#4a4a4a')
   strokeWeight(1)
-  for (let i = 1; i <= 5; ++i) {
-    line(100, i * 100, 500, i * 100)
-    line(i * 100, 100, i * 100, 500)
+  for (let i = 0; i < 5; ++i) {
+    drawLine(0, i * 100, 400, i * 100)
+    drawLine(i * 100, 0, i * 100, 400)
   }
 
   length = 200
   for (let i = 0; i < 5; ++i) {
     for (let j = 0; j < length; ++j) {
-      targetX = pointPos - j + 100;
-      if (targetX < 100) {
+      targetX = pointPos - j;
+      if (targetX < 0) {
         targetX += 400
       }
-      targetY = (i * 100) + 100;
+      targetY = i * 100;
       targetColor = color(255, 255, 255, 255 * (1 - (Math.abs((0.5 * length) - j) / (0.5 * length))));
       stroke(targetColor)
 
@@ -86,7 +86,7 @@ function drawImpl() {
         } else {
           strokeWeight(2)
         }
-        point(targetX, targetY);
+        drawPoint(targetX, targetY);
       }
 
       tmp = targetX
@@ -98,18 +98,10 @@ function drawImpl() {
         } else {
           strokeWeight(2)
         }
-        point(targetX, targetY)
+        drawPoint(targetX, targetY)
       }
     }
   }
-
-  noStroke()
-  drawPanel(1, 3, 60, l1Img, '#93d7d8')
-  drawPanel(3, 1, 66, l2Img, '#95d6ba')
-  drawPanel(2, 0, 70, l3Img, '#f8e3a9')
-  drawPanel(0, 0, 80, l4Img, '#f89c7f')
-  drawPanelL(0, 1, 140, l5Img, '#779cb2')
-  drawPanelL(2, 2, 170, l6Img, '#fff3f0')
 }
 
 function isValidPoint(x, y) {
@@ -117,36 +109,58 @@ function isValidPoint(x, y) {
 }
 
 function isInBoard(x, y) {
-  return 100 <= targetX && targetX <= 500 &&
-    100 <= targetY && targetY <= 500
+  return 0 <= targetX && targetX <= 400 &&
+    0 <= targetY && targetY <= 400
 }
 
 function isUnicBoard(x, y) {
   margin = 1
-  return 100 + margin <= targetX && targetX <= 300 - margin &&
-    200 + margin <= targetY && targetY <= 400 - margin ||
-    300 + margin <= targetX && targetX <= 500 - margin &&
-    300 + margin <= targetY && targetY <= 500 - margin
+  return 0 + margin <= targetX && targetX <= 200 - margin &&
+    100 + margin <= targetY && targetY <= 300 - margin ||
+    200 + margin <= targetX && targetX <= 400 - margin &&
+    200 + margin <= targetY && targetY <= 400 - margin
+}
+
+function drawPanels() {
+  noStroke()
+  drawPanel(1, 3, 60, l1Img, '#93d7d8')
+  drawPanel(3, 1, 66, l2Img, '#95d6ba')
+  drawPanel(2, 0, 70, l3Img, '#f8e3a9')
+  drawPanel(0, 0, 80, l4Img, '#f89c7f')
+  drawPanelL(0, 1, 140, l5Img, '#779cb2')
+  drawPanelL(2, 2, 170, l6Img, '#f595ae')
 }
 
 function drawPanel(i, j, size, img, color) {
   x = ((i * 100) - 1) + ((100 - size) * 0.5)
   y = ((j * 100) - 1) + ((100 - size) * 0.5)
   y -= 2
-  x += 100
-  y += 100
   fill(color)
-  rect(x, y + 5, size, size, size * 0.125)
-  image(img, x, y, size, size)
+  drawRect(x, y + 5, size, size, size * 0.125)
+  drawImage(img, x, y, size, size)
 }
 
 function drawPanelL(i, j, size, img, color) {
   x = ((i * 100) - 1) + ((200 - size) * 0.5)
   y = ((j * 100) - 1) + ((200 - size) * 0.5) 
   y -= 4
-  x += 100
-  y += 100
   fill(color)
-  rect(x, y + 8, size, size, size * 0.125)
-  image(img, x, y, size, size)
+  drawRect(x, y + 8, size, size, size * 0.125)
+  drawImage(img, x, y, size, size)
+}
+
+function drawPoint(x, y) {
+  point(x + BoardOffsetX, y + BoardOffsetY)
+}
+
+function drawLine(x1, y1, x2, y2) {
+  line(x1 + BoardOffsetX, y1 + BoardOffsetY, x2 + BoardOffsetX, y2 + BoardOffsetY)
+}
+
+function drawRect(x, y, w, h, r) {
+  rect(x + BoardOffsetX, y + BoardOffsetY, w, h, r)
+}
+
+function drawImage(img, x, y, w, h) {
+  image(img, x + BoardOffsetX, y + BoardOffsetY, w, h)
 }
